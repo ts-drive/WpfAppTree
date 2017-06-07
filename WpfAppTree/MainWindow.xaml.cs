90a1aceb215b01da7a1f4ccf587916cfcdf3182c
ts-drive <ts-drive@yandex.ru>
2017-06-07 10:14:26 +0500

    Add project files.

---
+++ WpfAppTree/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FirstWpfApp
+{
+    public partial class MainWindow : Window
+    {
+#region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+#endregion
+
+#region OnLoaded
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem  root=new TreeViewItem();
+            TreeViewItem child = new TreeViewItem();
+            root.Items.Add(child);
+            FolderView.Items.Add(root);
+        }
+#endregion
+    }
+}
